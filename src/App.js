f3deb23d8a4801872a80879252b44b00a6daa44b
zakariachatria <zakariachatria@hotmail.fr>
2018-03-09 23:21:35 +0000

    added Component AddString to add a couple key/value of type string using handleAddValue

--- src/App.js
+++ src/App.js
@@ -2,17 +2,34 @@ import React, { Component } from 'react';
 
 class App extends Component {
     state = {
-        type: 'string'
+        type: 'string',
+        jsonobjet: {}
     };
     handleChangeType = (e) => {
         this.setState({type: e.target.value});
+    };
+    handleAddValue = (e) => {
+        //type String
+        e.preventDefault();
+        const key = e.target.elements.key.value.trim();
+        let jsonobjet2 = this.state.jsonobjet;
+
+        jsonobjet2[key] = e.target.elements.valeur.value.trim();
+
+            this.setState(() => ({jsonobjet: jsonobjet2}));
+
+
     };
   render() {
     return (
       <div>
           <h1>JSON MAKER</h1>
           <ChooseType handleChangeType={this.handleChangeType}/>
-
+          <AddString
+              handleAddValue={this.handleAddValue}
+              type={this.state.type}
+          />
+          {JSON.stringify(this.state.jsonobjet) && <pre>{JSON.stringify(this.state.jsonobjet)}</pre>}
       </div>
     );
   }
@@ -28,6 +45,30 @@ class ChooseType extends Component {
                     <option value='number'>number</option>
                 </select>
             </form>
+        );
+    }
+}
+class AddString extends Component {
+    render() {
+        return (
+            <div>
+
+                <form onSubmit={this.props.handleAddValue}>
+
+                    <br/>
+                    <label forhtml="key">enter the key : </label>
+
+                    <input type="text" name='key'/>
+                    <br/>
+                    <label forhtml="value">enter a {this.props.type} : </label>
+
+                    {<input type="text" name='valeur'/>}
+
+
+                    <br/>
+                    <button>Submit</button>
+                </form>
+            </div>
         );
     }
 }
