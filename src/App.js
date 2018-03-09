7052b94d81c41fede33229a79b26c706161003b4
zakariachatria <zakariachatria@hotmail.fr>
2018-03-09 22:32:31 +0000

    create component ChooseType to choose the type of key to add

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
 
-
 class App extends Component {
+    state = {
+        type: 'string'
+    };
+    handleChangeType = (e) => {
+        this.setState({type: e.target.value});
+    };
   render() {
     return (
       <div>
+          <h1>JSON MAKER</h1>
+          <ChooseType handleChangeType={this.handleChangeType}/>
 
       </div>
     );
   }
+}
+class ChooseType extends Component {
+    render() {
+        return (
+            <form>
+                <label forhtml="type">choose type to add : </label>
+                <select onChange={this.props.handleChangeType}>
+                    <option value='string'>string</option>
+                    <option value='boolean'>boolean</option>
+                    <option value='number'>number</option>
+                </select>
+            </form>
+        );
+    }
 }
 
 export default App;
